2323c46b5fe5c89d629542a17016b5fbf11a9346
SalahidinAI <zulpukarovz555@gmail.com>
2025-05-11 15:05:10 +0600

    arduino

--- arduino/car_control.ino
+++ arduino/car_control.ino
@@ -1,109 +1,118 @@
-#include <Servo.h>
 #include <SoftwareSerial.h>
+#include <Servo.h>
 
-// Пины для драйвера L298N
-const int motor1Pin1 = 4;  // IN1
-const int motor1Pin2 = 5;  // IN2
-const int motor2Pin1 = 6;  // IN3
-const int motor2Pin2 = 7;  // IN4
+SoftwareSerial sim800(10, 11); // RX, TX
+Servo steeringServo;
 
-// Пин для сервопривода руля
+const int motorPin1 = 4;
+const int motorPin2 = 5;
+const int motorPin3 = 6;
+const int motorPin4 = 7;
 const int servoPin = 9;
 
-// Пины для SIM800L
-const int sim800lTx = 10;
-const int sim800lRx = 11;
-
-// Создаем объекты
-Servo steeringServo;
-SoftwareSerial sim800l(sim800lRx, sim800lTx);
-
-// Переменные для хранения команд
-String currentCommand = "STOP";
-int steeringAngle = 90;  // Центральное положение руля
+String lastCommand = "";
+unsigned long lastRequestTime = 0;
+const unsigned long REQUEST_INTERVAL = 2000; // Интервал запросов 2 секунды
 
 void setup() {
-  // Инициализация пинов драйвера
-  pinMode(motor1Pin1, OUTPUT);
-  pinMode(motor1Pin2, OUTPUT);
-  pinMode(motor2Pin1, OUTPUT);
-  pinMode(motor2Pin2, OUTPUT);
-  
-  // Инициализация сервопривода
-  steeringServo.attach(servoPin);
-  steeringServo.write(steeringAngle);
-  
-  // Инициализация Serial для отладки
   Serial.begin(9600);
-  
-  // Инициализация SIM800L
-  sim800l.begin(9600);
-  
-  // Остановка моторов при запуске
-  stopMotors();
+  sim800.begin(9600);
+  steeringServo.attach(servoPin);
+
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+
+  delay(3000); // Ждем подключения SIM
+  setupGPRS();
 }
 
 void loop() {
-  // Проверяем наличие данных от SIM800L
-  if (sim800l.available()) {
-    String command = sim800l.readStringUntil('\n');
-    command.trim();
-    processCommand(command);
-  }
+  unsigned long currentTime = millis();
   
-  // Проверяем наличие данных от Serial для отладки
-  if (Serial.available()) {
-    String command = Serial.readStringUntil('\n');
-    command.trim();
-    processCommand(command);
+  // Проверяем, прошло ли достаточно времени с последнего запроса
+  if (currentTime - lastRequestTime >= REQUEST_INTERVAL) {
+    lastRequestTime = currentTime;
+    
+    // Отправляем HTTP-запрос
+    sim800.println("AT+HTTPACTION=0");
+    delay(1000); // Уменьшаем время ожидания
+
+    // Читаем ответ
+    sim800.println("AT+HTTPREAD");
+    delay(500);
+
+    String response = "";
+    while (sim800.available()) {
+      response += sim800.readString();
+    }
+
+    // Извлекаем команду из ответа
+    int index = response.indexOf("\r\n") + 2;
+    if (index > 2) { // Проверяем, что нашли начало команды
+      String cmd = response.substring(index, response.indexOf("\r\n", index));
+      cmd.trim();
+      
+      // Обрабатываем команду только если она изменилась
+      if (cmd != lastCommand) {
+        processCommand(cmd);
+        lastCommand = cmd;
+      }
+    }
   }
 }
 
-void processCommand(String command) {
-  if (command == "FORWARD") {
-    moveForward();
-  } else if (command == "BACKWARD") {
-    moveBackward();
-  } else if (command == "LEFT") {
-    turnLeft();
-  } else if (command == "RIGHT") {
-    turnRight();
-  } else if (command == "STOP") {
-    stopMotors();
+void setupGPRS() {
+  sendAT("AT");
+  sendAT("AT+SAPBR=3,1,\"Contype\",\"GPRS\"");
+  sendAT("AT+SAPBR=3,1,\"APN\",\"internet\"");
+  sendAT("AT+SAPBR=1,1");
+  delay(2000);
+  sendAT("AT+SAPBR=2,1");
+  sendAT("AT+HTTPINIT");
+  sendAT("AT+HTTPPARA=\"CID\",1");
+  sendAT("AT+HTTPPARA=\"URL\",\"http://13.60.251.21:8000/get-command\"");
+}
+
+void sendAT(String cmd) {
+  sim800.println(cmd);
+  delay(1000);
+  while (sim800.available()) {
+    Serial.println(sim800.readString());
   }
 }
 
-void moveForward() {
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin1, HIGH);
-  digitalWrite(motor2Pin2, LOW);
-}
-
-void moveBackward() {
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, HIGH);
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, HIGH);
-}
-
-void turnLeft() {
-  steeringAngle = 45;  // Поворот руля влево
-  steeringServo.write(steeringAngle);
-  moveForward();
-}
-
-void turnRight() {
-  steeringAngle = 135;  // Поворот руля вправо
-  steeringServo.write(steeringAngle);
-  moveForward();
-}
-
-void stopMotors() {
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, LOW);
-  digitalWrite(motor2Pin1, LOW);
-  digitalWrite(motor2Pin2, LOW);
-  steeringAngle = 90;  // Возврат руля в центральное положение
-  steeringServo.write(steeringAngle);
+void processCommand(String cmd) {
+  if (cmd == "FORWARD") {
+    digitalWrite(motorPin1, HIGH);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, HIGH);
+    digitalWrite(motorPin4, LOW);
+    steeringServo.write(90);
+  } else if (cmd == "BACKWARD") {
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, HIGH);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, HIGH);
+    steeringServo.write(90);
+  } else if (cmd == "LEFT") {
+    digitalWrite(motorPin1, HIGH);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, LOW);
+    steeringServo.write(0);
+  } else if (cmd == "RIGHT") {
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, HIGH);
+    digitalWrite(motorPin4, LOW);
+    steeringServo.write(180);
+  } else {
+    digitalWrite(motorPin1, LOW);
+    digitalWrite(motorPin2, LOW);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, LOW);
+    steeringServo.write(90);
+  }
 } 
