5b287c09b3739b6006fb3f4882e43aed672c1fbf
Salah <157966023+SalahidinAI@users.noreply.github.com>
2025-05-14 14:21:34 +0600

    Update server.py
    aaa

--- server.py
+++ server.py
@@ -30,4 +30,3 @@ async def set_command(data: CommandRequest):
     current_command = data.command
     logger.info(f"Command updated to: {current_command}")
     return {"status": "ok", "command": current_command}
-
